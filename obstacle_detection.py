c160dab55ae7bcbb998d01723fc0ed6ba0c1ff27
raghav <ruppala20@gmail.com>
2026-02-07 13:21:04 -0500

    added DBSCAN

--- obstacle_detection.py
+++ obstacle_detection.py
@@ -1,8 +1,12 @@
 import pyzed.sl as sl
 import numpy as np
+import open3d as o3d
 import sys
 
 def main():
+    safety = 8
+    safety_sq = 64
+
     zed = sl.Camera()
 
     init_params = sl.InitParameters()
@@ -50,13 +54,44 @@ def main():
           # Retrieve spatial_map when ready
           if zed.get_spatial_map_request_status_async() == sl.ERROR_CODE.SUCCESS and timer > 0:
              zed.retrieve_spatial_map_async(point_cloud)
-             points = point_cloud.get_points()
+             points = point_cloud.vertices()
              if points.size == 0:
                  continue
              xyz = points[:, :3]
              xyz = xyz[np.isfinite(xyz).all(axis=1)]
              xyz = np.ascontiguousarray(xyz, dtype=np.float32)
-             print("XYZ point cloud shape:", xyz.shape)
+
+             #selected_points = []
+
+             #for point in xyz:
+             #    x = point[0]
+             #    y = point[1]
+             #    z = point[2]
+
+             #    if (x >= safety or y >= safety or z >= safety):
+             #        continue;
+             #    if ((x**2 + y**2 + z**2) > safety_sq):
+             #        continue;
+             #    selected_points.append([x, y, z])
+
+            selected_points = np.array([point for point in xyz 
+                            if x < safety and y < safety and z < safety 
+                            and (point[0]**2 + point[1]**2 + point[2]**2) < safety_sq],
+                           dtype=np.float32)
+
+            pcd = o3d.geometry.PointCloud()
+            pcd.points = o3d.utility.Vector3dVector(selected_points)
+
+            with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Error) as cm:
+                labels = np.array(pcd.cluster_dbscan(eps=0.2, min_points=10))
+
+            num_objects = labels.max() + 1
+            print("Detected " + num_objects + " objects")
+
+            for i in range(num_objects):
+                cluster_points = np.asarray(pcd.points)[labels == i]
+                centroid = cluster_points.mean(axis=0)
+                print("Object " + i + ": centroid " + centroid)
 
 
           timer += 1
