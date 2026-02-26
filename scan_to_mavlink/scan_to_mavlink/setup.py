c5b3c76c6860e6512357a18842e93a685a61d47e
AdvayRChandra <advay.chandra@protonmail.com>
2026-02-25 20:05:06 -0500

    Bump version to 0.4.0 and update package metadata; add scan_to_mavlink package with MAVLink support

--- scan_to_mavlink/scan_to_mavlink/setup.py
+++ scan_to_mavlink/scan_to_mavlink/setup.py
@@ -4,7 +4,7 @@ package_name = 'scan_to_mavlink'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.4.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,10 +13,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
+    maintainer='Vertical Flight Systems Purdue',
     maintainer_email='vfspurdue@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Converts LaserScan data to MAVLink obstacle distance messages',
+    license='MIT',
     extras_require={
         'test': [
             'pytest',
