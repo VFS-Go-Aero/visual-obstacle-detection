b213112396faf004605d1bca269160cdb5510a9c
AdvayRChandra <advay.chandra@protonmail.com>
2026-04-15 18:46:55 -0400

    Fix data_files formatting in setup.py for clarity

--- visual_obstacle_detection/setup.py
+++ visual_obstacle_detection/setup.py
@@ -10,11 +10,8 @@ setup(
     packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
-        (
-            "share/" + package_name,
-            ["package.xml", "visual_obstacle_detection/excluded_boxes.xml"],
-            (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
-        ),
+        ("share/" + package_name, ["package.xml", "visual_obstacle_detection/excluded_boxes.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
