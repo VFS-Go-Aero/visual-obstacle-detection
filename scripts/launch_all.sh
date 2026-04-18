e15fe82fc938e51c6f232ed9f08d036c545e98fb
AdvayRChandra <advay.chandra@protonmail.com>
2026-04-18 14:08:33 -0400

    Add launch commands for multi and single ZED configurations in launch_all.sh

--- scripts/launch_all.sh
+++ scripts/launch_all.sh
@@ -1,1 +1,3 @@
 ros2 launch mavros apm.launch fcu_url:=/dev/ttyACM0:115200 gcs_url:=udp://@10.186.18.209:14550
+ros2 launch --packages-select launch_files multi_zed.launch.py
+ros2 launch --packages-select visual_obstacle_detection single_zed.launch.py
